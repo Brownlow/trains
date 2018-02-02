7f656ae8988091d43bf93488e8760a6f23f05c9e
Ryan B <ryan@rbdesignd.com>
2018-02-01 18:33:32 -0800

    finish up momentjs stuff

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,58 +21,68 @@ $('#addTrain').on('click', function(event){
 
 	event.preventDefault();
 
-	var newTrain = $('<tr id="train"></tr>');
-	$('#row').append(newTrain);
-
-
-
 	var trainName = $('#trainName').val().trim();
 	var destination = $('#destination').val().trim();
 	var firstTrain = $('#firstTrain').val().trim();
 	var frequency = $('#frequency').val().trim();
 
-
-	// Change what is saved in firebase
-      database.ref().push({
-        name: trainName,
+	var newTrain = {
+		name: trainName,
         destination: destination,
         firstTrain: firstTrain,
         frequency, frequency
-      });
-    
+	}
 
+	// Change what is saved in firebase
+    database.ref().push(newTrain);
 
-	// $('#name-display').append(trainName);
-	// $('#destination-display').append(destination);
-	// $('#first-display').append(firstTrain);
-	// $('#next-display').append(frequency);
+    // empty form fields
+    $('#trainName').val('');
+    $('#destination').val('');
+    $('#firstTrain').val('');
+    $('#frequency').val('');
 
 });
 
-database.ref().on("value", function(snapshot) {
+database.ref().on("child_added", function(childSnapshot, prevChildKey) {
 
-      // Print the initial data to the console.
-      console.log(snapshot.val());
+	// Create variables so its easier to use the data
+	var trainName = childSnapshot.val().name;
+	var trainDest = childSnapshot.val().destination;
+	var firstTrain = childSnapshot.val().firstTrain;
+	var trainFreq = childSnapshot.val().frequency;
 
-      // Log the value of the various properties
-      console.log(snapshot.val().name);
-      console.log(snapshot.val().destination);
-      console.log(snapshot.val().firstTrain);
-      console.log(snapshot.val().frequency);
 
-      // Change the HTML
-      //$("#row").text(snapshot.val().name + " | " + snapshot.val().destination + " | " + snapshot.val().firstTrain + " | " + snapshot.val().frequency);
-      $("#row").append('<td id="name-display">');
-      $("#row").append('<td id="destination-display">');
-      $("#row").append('<td id="first-display">');
-      $("#row").append('<td id="frequency-display">');
-      $('#name-display').text(snapshot.val().name);
-      $('#destination-display').text(snapshot.val().destination);
-      $('#first-display').text(snapshot.val().firstTrain);
-      $('#frequency-display').text(snapshot.val().frequency);
+  // First Time (pushed back 1 year to make sure it comes before current time)
+  var firstTimeConverted = moment(firstTrain, "hh:mm").subtract(1, "years");
+  console.log(firstTimeConverted);
 
-      // If any errors are experienced, log them to console.
+  // Current Time
+  var currentTime = moment();
+  console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+
+  // Difference between the times
+  var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+  console.log("DIFFERENCE IN TIME: " + diffTime);
+
+  // Time apart (remainder)
+  var tRemainder = diffTime % trainFreq;
+  console.log(tRemainder);
+
+  // Minute Until Train
+  var tMinutesTillTrain = trainFreq - tRemainder;
+  console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+  // Next Train
+  var nextTrain = moment().add(tMinutesTillTrain, "minutes").format("hh:mm");
+  console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+
+  // Change the HTML to add the data from firebase
+  $('tbody').append('<tr><td>' + trainName +'</td>' + '<td>' + trainDest +'</td>' + '<td>' + trainFreq +'</td>' + '<td>' + nextTrain +'</td>' + '<td>' + tMinutesTillTrain +'</td></tr>');
+
+    // If any errors are experienced, log them to console.
     }, function(errorObject) {
       console.log("The read failed: " + errorObject.code);
     });
+
 
